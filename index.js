e3f5966d54581c65aa511168cfb4d7a433e345ee
Jorge Hernandez <jored187@gmail.com>
2015-07-09 21:47:20 -0430

    Revert "Revert "Revert "cambios"""
    This reverts commit 91fe9cf55fb1c0dbb47a99c7d365cb860bb08b52.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var server= require("./server");
-var router= require("./router");
 
 server.inicio();
 
--- router.js
+++
@@ -1,5 +0,0 @@
-function route(pathname) {
-  console.log("A punto de rutear una peticion para " + pathname);
-}
-
-exports.route = route;
